8ea325469b3ee974aa1370c1ac5f2e8c6651d891
Valentin Dornauer <valentin@unimplemented.org>
2020-08-28 02:38:08 +0200

    Skip empty 100% alpha tiles completely

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/schollz/progressbar"
 	"image"
 	"image/draw"
 	"image/png"
@@ -59,11 +60,13 @@ func main() {
 	for z := source.minZ; z <= source.maxZ; z++ {
 		zPart := fmt.Sprintf("/%d/", z)
 		zBasePath := source.basePath + zPart
-		log.Printf("Entering z=%s\n", zBasePath)
+		//log.Printf("Entering z=%s\n", zBasePath)
 		xDirs, err := ioutil.ReadDir(zBasePath)
 		if err != nil {
 			panic(err)
 		}
+		log.Printf("Zoom level %d/%d", z, source.maxZ)
+		bar := progressbar.Default(int64(len(xDirs)))
 		for _, x := range xDirs {
 			if x.IsDir() {
 				xNum, err := strconv.Atoi(x.Name())
@@ -72,21 +75,29 @@ func main() {
 				}
 				xPart := fmt.Sprintf("%s%d/", zPart, xNum)
 				xBasePath := source.basePath + xPart
-				log.Printf("Entering x=%s\n", xBasePath)
+				//log.Printf("Entering x=%s\n", xBasePath)
 				yFiles, err := ioutil.ReadDir(xBasePath)
 				if err != nil {
 					panic(err)
 				}
 				for _, y := range yFiles {
-					processInputTile(source.basePath, destDesc.basePath, xPart+y.Name())
+					err := os.MkdirAll(destDesc.basePath+xPart, os.ModePerm)
+					if err != nil {
+						panic(err)
+					}
+					err = processInputTile(source, destDesc, xPart+y.Name())
+					if err != nil {
+						panic(err)
+					}
 				}
 			}
+			bar.Add(1)
 		}
 	}
 }
 
-func processInputTile(sourcePath, destPath, relTilePath string) (err error) {
-	f, err := os.Open(sourcePath + relTilePath)
+func processInputTile(source, dest TilesetDescriptor, relTilePath string) (err error) {
+	f, err := os.Open(source.basePath + relTilePath)
 	if err != nil {
 		return
 	}
@@ -97,57 +108,70 @@ func processInputTile(sourcePath, destPath, relTilePath string) (err error) {
 		return
 	}
 
-	if hasAlphaPixel(img) {
-		log.Println(destPath + relTilePath)
-		destF, err := os.Open(destPath + relTilePath)
+	hasAlphaPixel, skip := analyzeAlpha(img)
+
+	if skip {
+		return nil
+	}
+
+	// if the front image has at least one transparent pixel (and exists), merge front and back
+	_, err = os.Stat(dest.basePath + relTilePath)
+	if hasAlphaPixel && err == nil {
+		//log.Println(dest.basePath + relTilePath)
+		destF, err := os.Open(dest.basePath + relTilePath)
 		if err != nil {
-			panic(err)
+			return err
 		}
 
 		destImg, _, err := image.Decode(destF)
 		if err != nil {
-			panic(err)
+			return err
 		}
 		output := image.NewRGBA(image.Rect(0, 0, destImg.Bounds().Max.X, destImg.Bounds().Max.Y))
-		draw.Draw(output, img.Bounds(), img, image.Point{0, 0}, draw.Over)
 		draw.Draw(output, destImg.Bounds(), destImg, image.Point{0, 0}, draw.Over)
+		draw.Draw(output, img.Bounds(), img, image.Point{0, 0}, draw.Over)
 		destF.Close()
 
-		destF, err = os.Create(destPath + relTilePath)
+		destF, err = os.Create(dest.basePath + relTilePath)
 		if err != nil {
-			panic(err)
+			return err
 		}
 		err = png.Encode(destF, output)
 		if err != nil {
-			panic(err)
+			return err
 		}
 	} else {
-		// Simply overwrite the destination file completely
+		// if the front tile completely occludes the back tile, just replace it
 		f.Seek(0, io.SeekStart)
-		outfile, err := os.Create(destPath + relTilePath)
+		outfile, err := os.Create(dest.basePath + relTilePath)
 		defer outfile.Close()
 		if err != nil {
-			panic(err)
+			return err
 		}
 		_, err = outfile.ReadFrom(f)
 		if err != nil {
-			panic(err)
+			return err
 		}
 	}
 
 	return nil
 }
 
-func hasAlphaPixel(img image.Image) (hasAlpha bool) {
+func analyzeAlpha(img image.Image) (hasAlphaPixel, skip bool) {
+	skip = true
+	hasAlphaPixel = false
 	for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y++ {
 		for x := img.Bounds().Min.X; x < img.Bounds().Max.X; x++ {
 			_, _, _, a := img.At(x, y).RGBA()
-			if a != 255 {
-				return true
+			if a == 65535 {
+				skip = false
+			} else {
+				hasAlphaPixel = true
 			}
+			// XXX could return early if one non-alpha pixel and one alpha pixel has been found.
 		}
 	}
-	return false
+	return hasAlphaPixel, skip
 }
 
 type TilesetDescriptor struct {
